8fe26edf0e47ce2b4d5d08475a65d2111e1e5be5
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-01-05 15:19:57 -0300

    Delete seller

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -6,6 +6,7 @@ namespace SalesWebMVC.Models
 {
     public class Seller
     {
+
         public int ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
